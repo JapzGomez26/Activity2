669c010bd3fcb2e55b0a55722b9a71466536d2e2
JapzGomez26 <philipgomez26@gmail.com>
2024-09-22 01:48:31 +0800

    fixedtheheader

--- routes/router.js
+++ routes/router.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const controller = require('../controller/UserController.js');
+const controller = require('../controller/UserController');
 
 router.get('/', controller.index);
 
